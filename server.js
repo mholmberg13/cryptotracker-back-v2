415c322074d4623a54d9fa0c63aa198de3307f80
Matt Holmberg <mholmberg13@gmail.com>
2020-06-23 20:39:48 -0500

    working CORS issue

--- server.js
+++ server.js
@@ -23,6 +23,14 @@ mongoose.connection.once('open', ()=> {
     console.log('connected to mongoose...');
 })
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 // middleware
 app.use(express.json());
 
@@ -51,7 +59,7 @@ const whitelist = [
       }
     },
   };
-app.use(cors());
+app.use(cors(corsOptions));
 
 // Mount the routes from users controller
 app.use('/crypto', usersController)
